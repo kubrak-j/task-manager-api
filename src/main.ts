05e099e8414c6dec0a88e88a8a308c654bfdbb9b
Jaroslav Kubrak <jarikkubrak@gmail.com>
2026-05-21 19:51:11 +0200

    feat: add task interface, mock data, and GET /tasks endpoint

--- src/main.ts
+++ src/main.ts
@@ -3,11 +3,27 @@ import express from "express";
 const app = express();
 const port = 8000;
 
+interface Task {
+    id: number;
+    title: string;
+    difficult: string;
+    completed: boolean;
+};
+
+const tasks: Task[] = [
+    {id: 1, title: `walk the dog`, difficult: `easy`, completed: false},
+    {id: 2, title: `go to the gym`, difficult: `hard`, completed: false},
+    {id: 3, title: `take a shower`, difficult: `easy`, completed: false},
+    {id: 4, title: `read a chapter of book`, difficult: `medium`, completed: false},
+    {id: 5, title: `practice French`, difficult: `hard`, completed: false}
+];
+
 app.listen(8000, () => {
     console.log(`The server is running`);
     console.log(`listening on port ${port}`);
 });
 
-app.get(`/`, (req, res) => {
-    res.send(`hello world!`);
+app.get(`/tasks`, (req, res) => {
+
+    res.json(tasks);
 });
